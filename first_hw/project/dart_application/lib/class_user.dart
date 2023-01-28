aa35137190036e173f62cd5a151b496f032d1349
Mr.Fill <mr.filldev@gmail.com>
2023-01-28 21:17:21 +0300

    переименование списка, добавление комментариев

--- first_hw/project/dart_application/lib/class_user.dart
+++ first_hw/project/dart_application/lib/class_user.dart
@@ -18,35 +18,36 @@ class GeneralUser extends User {
 }
 
 class UserManager<T extends User> {
-  final email = <String>['a.corpE.iwanaev23@work.com', 'u.KachalovV@user.com'];
+  // список пользователей, в которых хранятся как пользователи, так и администраторы
+  final users = <String>['a.corpE.iwanaev23@work.com', 'u.KachalovV@user.com'];
+  
 
   void addEmail() {
     print('\nЭл. почта добавлена.');
-    email.add('u.Email@gmail.com');
-    email.add('u.Email2@gmail.com');
-    email.add('u.Email3@gmail.com');
-    email.add('a.corpE.worker@gmail.com');
-    email.add('a.corpE.worker@gmail.com');
-    email.add('a.corpE.worker@gmail.com');
+    users.add('u.Email@gmail.com');
+    users.add('u.Email2@gmail.com');
+    users.add('u.Email3@gmail.com'); // пользователь
+    users.add('a.corpE.worker@gmail.com'); // администратор
+    users.add('a.corpE.worker@gmail.com');
+    users.add('a.corpE.worker@gmail.com');
   }
 
   void deleteEmail(int indexEmail) {
-    email.removeAt(indexEmail - 1);
+    users.removeAt(indexEmail - 1);
     print(
-        '\nЭл. почта ${email[indexEmail - 1]} удалена.'); //Соответствено можно было реализовать отображение почты админа или обычного пользователя
+        '\nЭл. почта ${users[indexEmail - 1]} удалена.'); //Соответствено можно было реализовать отображение почты админа или обычного пользователя
   }
   
   void listEmails() {
     print('\nСписок email:');
-    for (var i = 0; i < email.length; i++) {
-      var emails = email[i].split('E')[0];
-      if (emails == 'a.corp') {
-        print('email #${i + 1}: ${email[i].split('@')[1]}');
+    for (var i = 0; i < users.length; i++) {
+      var emails = users[i].split('E')[0];
+      if (emails == 'a.corp') { // проверка на администратора
+        print('email #${i + 1}: ${users[i].split('@')[1]}');
       } else {
-        print('email #${i + 1}: ${email[i]}');
+        print('email #${i + 1}: ${users[i]}');
       }
     }
   }
 }
-
 
